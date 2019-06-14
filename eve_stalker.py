bf13b345f250a77b3e25e1266fb549980f998eed
Stefan Helms <me@stefanhelms.com>
2019-06-14 22:10:14 +0200

    Successfully implemented zkillboard stream into discord server. Added file to test filters on in the future

--- eve_stalker.py
+++ eve_stalker.py
@@ -1,15 +1,15 @@
 import discord
-import aiohttp
 import requests
 import asyncio
-import websocket
 import websockets
+import json
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client)) 
+    print('We have logged in as {0.user}'.format(client))
+    await connect_Zkill_wss()
 
 @client.event
 async def on_message(message):
@@ -23,17 +23,17 @@ async def connect_Zkill_wss():
             'wss://zkillboard.com:2096') as websocket:
             name = ('{"action":"sub","channel":"killstream"}')
 
-
             await websocket.send(name)
-            print(name)
+            print('Requesting zkill stream..')
             while True:
                 response = await websocket.recv()
-                print(response)
+                global data
+                data = json.loads(response)
+                zkill_url = data['zkb']['url']
+                print(zkill_url)
+                zkill_channel = client.get_channel(589180865016365066)
+                await zkill_channel.send(zkill_url)
                 await asyncio.sleep(0)
-                break
-
-
-
-asyncio.run(connect_Zkill_wss())
-
 client.run('NTg5MDY5MDk0NzcxMjI4Njky.XQOTvA.BvSBGpO9KwMSTlu1Zk7oI1J3W5U')
+
+
