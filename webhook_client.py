9bbff146e556aa47158cbf8c7b8a7d1e49d3da7b
Stefan Helms <me@stefanhelms.com>
2019-06-21 14:24:54 +0200

    ip update

--- webhook_client.py
+++ webhook_client.py
@@ -3,13 +3,13 @@ import websockets
 
 async def hello():
     async with websockets.connect(
-        'ws://stefanhelms.com:8765') as websocket:
-    name = input("What's your name? ")
+             'ws://stefanhelms.com:8765') as websocket:
+        name = input("What's your name? ")
 
-    await websocket.send(name)
-    print(f"> {name}")
+        await websocket.send(name)
+        print(f"> {name}")
 
-    greeting = await websocket.recv()
-                                                    print(f"< {greeting}")
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
 
 asyncio.get_event_loop().run_until_complete(hello())
